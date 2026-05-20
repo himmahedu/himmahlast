dc2a709fd35e57747f7ada31ff046cbd389b948a
همة التعليمية <himmahedu@gmail.com>
2026-05-20 07:38:38 +0300

    Delete lib/env.dart

--- lib/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
-@Envied(path: '.env')
-abstract class Env {
-  @EnviedField(varName: 'SERVER_URL', obfuscate: true)
-  static String serverUrl = _Env.serverUrl;
-}
